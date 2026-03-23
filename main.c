c6138c8d60da0b386f7b23b60dd76a9692060fdf
luccabracco <luccabraccoferreira@gmail.com>
2026-03-23 11:42:50 -0300

    Exercicios Finalizados

--- main.c
+++ main.c
@@ -1,2 +1,166 @@
-#include <stdio.h>
+// Atividade Extra
 
+// OBS: Alguns exercicios estão iguais
+
+// Exercicio 1 e 2
+int x = (2+3)*4;
+
+/*
+2 + 3 = 5
+5 * 4 = 20
+x = 20
+Multiplicação acontece antes da soma
+*/
+
+// Exercicio 3
+int x = 10/3;
+
+/*
+10 / 3 = 3
+% = 1
+x = 3
+Por não ter outra casa decimal no dividendo o computador da um resultado inteiro
+ignorando o 1 do resto
+*/;
+
+// Exercicio 4 e 5
+int x = 10/3.0;
+
+/*
+10 / 3.0 = 3.333...
+x = 3.333...
+Agora por ter outras casas decimais no dividendo o computador dá o reusltado 
+com as casas decimais
+*/;
+
+// Exercicio 6
+int x = 5+7/2;
+
+/*
+7 / 2 = 3
+5 + 3 = 8
+x = 8
+Divisao tem prioridade nessa conta e como o dividendo esta inteiro o computador
+ignora o 0.5 do resultado
+*/;
+
+// Exercicio 7
+int x = 10>5;
+
+/*
+x = 1
+10 > 5
+Resposta é verdadeira então x=1
+*/;
+
+// Exercicio 8
+int x = 10==5;
+
+/*
+x = 0
+Resposta é falsa então x=0
+*/;
+
+// Exercicio 9
+int x = (2+3)>(4*2);
+
+/*
+2 + 3 = 5
+4 * 2 = 8
+x = 0
+8 > 5
+Resposta é fasao entao x=0
+*/;
+
+// Exercicio 10
+int x = (10 / 2) + (3 > 1);
+
+/*
+10 / 2 = 5
+3 > 1 = 1
+5 + 1 = 6
+x = 6
+*/;
+
+// Exercicio 11
+int x = (5 > 3) * 10;
+
+/*
+5 > 3 = 1
+1 * 10 = 10
+x = 10
+*/;
+
+// Exercicio 12
+int x = (8 + 2) / (3 + 2);
+
+/*
+8 + 2 = 10
+3 + 2 = 5
+10 / 5 = 2
+x = 2
+*/;
+
+// Exercicio 14
+int x = (8 + 2) / 3 + 2;
+
+/*
+8 + 2 = 10
+10 / 3 = 3
+3 + 2 = 5
+x = 5
+Seguindo as ordens de prioridade
+*/;
+
+// Exercicio 15
+int x = 5 / 2 * 2;
+
+/*
+5 / 2 = 2
+2 * 2 = 4
+x = 4
+Não temos ordem de prioridade e sim de quem encontrou primeiro
+*/;
+
+// Exercicio 16
+int x = (10 > 5) + (3 * 2);
+
+/*
+10 > 5 = 1
+3 * 2 = 6
+1 + 6 = 7
+x = 7
+*/;
+
+// Exercicio 17
+int a, b;
+
+/*
+scanf("%d %d", &a, &b);
+int x = a + b * 2;
+
+a = 3;
+b = 4;
+
+
+4 * 2 = 8
+3 + 8 = 11
+Seguindo ordem de prioridade, mutiplicação antes
+*/;
+
+// Exercicio 18
+int idade;
+
+/*
+scanf("%d", &idade);
+int maior = idade >= 18;    
+
+if idade = 20;
+if idade = 15;
+
+
+20 > 18 
+x = 1
+15 > 18
+x = 0
+*/;
